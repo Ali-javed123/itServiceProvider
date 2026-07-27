7302c0783ef3d9f6980a55f1d1aa163d3926fcb3
ALi-javed123 <muhammadaliibrahim90@gmial.com>
2026-07-28 00:59:18 +0500

    DF

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const app = express();
 
 app.use(cors({
-   origin: "http://localhost:3000", // Next.js frontend
+   origin:["http://localhost:3000", "https://it-service-provider.vercel.app/"],  // Next.js frontend
 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
